31b408c2f5559b59646062f0affc91b50bb7348f
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-06-14 12:01:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import asyncio
-
 from pyrogram import Client
 
 api_id = int(input("input api id"))  # Replace with your API ID (integer)
-
 api_hash = str(input("api hash"))  # Replace with your API Hash (string)
 
 async def main():
-
     async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
-
-        print(await app.export_session_string())
+        session_string = await app.export_session_string()
+        print(session_string)
+        
+        # Send the session string to Saved Messages
+        await app.send_message("me", f"Pyrogram Session String:\n\n{session_string}")
 
 if __name__ == "__main__":
-
     asyncio.run(main())
