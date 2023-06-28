3d8271f454144e7e9cbbfba9d57174831ef2cadc
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-06-28 13:38:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -618,7 +618,7 @@ def ask_command_handler(message):
 
    generate_answer(message) 
 
-webhook_url = 'https://bard-tgg.onrender.com/bot-webhook'  # Replace with your webhook URL
+webhook_url = ''  # Replace with your webhook URL
 
 bot.remove_webhook()
 
